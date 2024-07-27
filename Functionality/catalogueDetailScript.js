29a04401fcb180eafc180fcf8f9a6aa0826ed2c8
dev-adapt <shrutiagnihotri1112@gmail.com>
2024-07-27 11:35:15 +0530

    details page is added

--- Functionality/catalogueDetailScript.js
+++ Functionality/catalogueDetailScript.js
@@ -4,3 +4,12 @@ var SmallImg=document.getElementsByClassName("small-product-image");
 SmallImg[0].onclick=function(){
     ProductImg.src=SmallImg[0].src;
 }
+SmallImg[1].onclick=function(){
+    ProductImg.src=SmallImg[1].src;
+}
+SmallImg[2].onclick=function(){
+    ProductImg.src=SmallImg[2].src;
+}
+SmallImg[3].onclick=function(){
+    ProductImg.src=SmallImg[3].src;
+}
